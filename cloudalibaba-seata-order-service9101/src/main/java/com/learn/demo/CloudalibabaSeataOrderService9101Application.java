57ee3951dc73523e2a6502b3a2c9832f5dc5f40c
jiaweiban <jiaweiban@163.com>
2020-09-24 23:15:50 +0800

    添加seata order模块

---
+++ cloudalibaba-seata-order-service9101/src/main/java/com/learn/demo/CloudalibabaSeataOrderService9101Application.java
@@ -0,0 +1,13 @@
+package com.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaSeataOrderService9101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaSeataOrderService9101Application.class, args);
+    }
+
+}
---
+++ cloudalibaba-seata-order-service9101/src/test/java/com/learn/demo/CloudalibabaSeataOrderService9101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudalibabaSeataOrderService9101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
